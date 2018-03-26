5789c533934ba22c8f4bb2b08fbd08d7047102b6
Tobias Lundgren <tobias@lundgrendesign.se>
2018-03-27 00:56:32 +0200

    Add grid system

---
+++ src/components/Grid/Hidden.js
@@ -0,0 +1,44 @@
+// @flow
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import styled from 'styled-components'
+import Row from './Row'
+import Col from './Col'
+import { withBreakpoints } from './BreakpointProvider'
+import { divvy, breakpoint, passOn } from '../../utils'
+
+type Props = {
+  children?: Array<React.Element<>>,
+  debug?: boolean,
+  xs?: boolean,
+  sm?: boolean,
+  md?: boolean,
+  lg?: boolean,
+  breakpoints?: Object,
+}
+
+function HiddenContainer(props: Props) {
+  const { children, debug, xs, sm, md, lg, breakpoints, ...rest } = props
+  const newChildren = passOn(children, [Row, Col], child => {
+    return {
+      debug:
+        typeof child.props.debug === 'undefined' ? debug : child.props.debug,
+    }
+  })
+  return <div {...rest}>{newChildren}</div>
+}
+
+const compute = name =>
+  breakpoint(
+    name,
+    (props, name) => `display:${props[name] ? 'none' : 'inherit'};`
+  )
+
+const Hidden = styled(HiddenContainer)`
+  ${compute('xs')}
+  ${compute('sm')}
+  ${compute('md')}
+  ${compute('lg')}
+`
+
+export default withBreakpoints(Hidden)
---
+++ src/components/Grid/index.js
@@ -0,0 +1,9 @@
+// @flow
+import Col from './Col'
+import Row from './Row'
+import Page from './Page'
+import Hidden from './Hidden'
+import BreakpointProvider, { withBreakpoints } from './BreakpointProvider'
+import * as utils from '../../utils'
+
+export { Col, Page, Row, Hidden, BreakpointProvider, withBreakpoints, utils }
